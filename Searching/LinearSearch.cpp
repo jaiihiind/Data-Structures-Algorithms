561bedca4cbc0b9bacd19e8b6a770d2c78096569
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-02-09 04:56:20 +0530

    Update LinearSearch.cpp

--- Searching/LinearSearch.cpp
+++ Searching/LinearSearch.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,arr[n], element;
 
+    bool found = 0; //todo > Its an element used b/c else stat. can't be used here >> it updates the value
+
+    cout<<"Enter size : ";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Enter your element to be searched: ";
+    cin>>element;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]==element)
+        {
+            cout<<"Element found!";
+            found=1;
+            break;          //b/c >>to avoid any duplicate elements
+        }
+    }
+
+    if(!found){
+        cout<<"element not found";
+    }
+    
+return 0;
+}
