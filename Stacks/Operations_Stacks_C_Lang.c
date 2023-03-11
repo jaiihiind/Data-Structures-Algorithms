c86d54f9e43636955bfd8e18e63f90ac65cc2f14
Rishav Ray <98175406+jaiihiind@users.noreply.github.com>
2023-03-11 20:28:08 +0530

    Add files via upload

---
+++ Stacks/Operations_Stacks_C_Lang.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 4
+
+int top = -1, array[SIZE];
+void push();
+void pop();
+void show();
+
+
+int main()
+{
+    int choice;
+    while (1)
+{
+        printf("\nPerform operations on the stack:");
+        printf("\n1.Push the element\n2.Pop the element\n3.Show\n4.End");
+        printf("\n\nEnter the choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            show();
+            break;
+        case 4:
+            exit(0);
+
+        default:
+            printf("\nInvalid choice!!");
+        }
+    }
+}
+
+void push()
+{
+    int data;
+    if (top == SIZE - 1){
+        printf("\nOverflow!!");
+    }
+    else{
+        printf("\nEnter the element to be added onto the stack: ");
+        scanf("%d", &data);
+        top = top + 1;
+        array[top] = data;
+    }
+}
+
+void pop()
+{
+    if (top == -1)
+    {
+        printf("\nUnderflow!!");
+    }
+    else
+    {
+        printf("\nPopped element: %d", array[top]);
+        top = top - 1;
+    }
+}
+
+void show()
+{
+    if (top == -1)
+    {
+        printf("\nUnderflow!!");
+    }
+    else
+    {
+        printf("\nElements present in the stack: \n");
+        for (int i = top; i >= 0; --i)
+            printf("%d\n", array[i]);
+    }
+}
